e155bd448930e10f714bdbc75274f3f9faf452bc
Jacqueline Herrald <jherrald@w-r6-p5.s19.be>
2019-12-17 13:23:22 +0100

    %s works +-

--- libftprintf.c
+++ libftprintf.c
@@ -6,11 +6,21 @@
 /*   By: jherrald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 16:48:44 by jherrald          #+#    #+#             */
-/*   Updated: 2019/12/17 13:00:33 by jherrald         ###   ########.fr       */
+/*   Updated: 2019/12/17 13:22:40 by jherrald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	x;
+
+	x = 0;
+	while (s[x])
+		x++;
+	return (x);
+}
 
 int		ft_printf(const char *coucou, ...)
 {
@@ -27,10 +37,17 @@ int		ft_printf(const char *coucou, ...)
 			if (coucou[x + 1] == 's')
 			{
 				value = va_arg(ap, char *);
-				write(1, value, 4);
+				write(1, value, ft_strlen(value));
+				return (0);
+			}
+			if (coucou[x + 1] == 'c')
+			{
+				value = va_arg(ap, char);
+				write(1, value, 1);
+				return (0);
 			}
 		}
-		write(1, &coucou[x], 1);		
+		write(1, &coucou[x], 1);
 		x++;
 	}
 
@@ -40,6 +57,10 @@ int		ft_printf(const char *coucou, ...)
 
 int main()
 {
-	ft_printf("test: %s", "kiko");
-	
+	char 	*temp = "kikouuuu";
+	char	*oups = "what";
+	char	a = 'c';
+//	ft_printf("test :%s %s", temp, oups);
+	ft_printf("test :%c", a);
+		
 }
