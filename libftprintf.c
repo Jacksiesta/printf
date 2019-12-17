5ce272961ca09df5345e68b328e07fafdd741cae
Jacqueline Herrald <jherrald@w-r6-p5.s19.be>
2019-12-17 13:01:57 +0100

    first try %s

--- libftprintf.c
+++ libftprintf.c
@@ -6,10 +6,40 @@
 /*   By: jherrald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 16:48:44 by jherrald          #+#    #+#             */
-/*   Updated: 2019/12/16 18:12:31 by jherrald         ###   ########.fr       */
+/*   Updated: 2019/12/17 13:00:33 by jherrald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "libftprintf.h"
 
-int		ft_printf(const char *, ...)
+int		ft_printf(const char *coucou, ...)
+{
+	int		x;
+	va_list	(ap);
+	char	*value;
+
+	x = 0;
+	va_start(ap, coucou); 
+	while (coucou[x])
+	{
+		if (coucou[x] == '%')
+		{
+			if (coucou[x + 1] == 's')
+			{
+				value = va_arg(ap, char *);
+				write(1, value, 4);
+			}
+		}
+		write(1, &coucou[x], 1);		
+		x++;
+	}
+
+	va_end(ap);
+	return (0);
+}
+
+int main()
+{
+	ft_printf("test: %s", "kiko");
+	
+}
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: jherrald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 18:07:46 by jherrald          #+#    #+#             */
-/*   Updated: 2019/12/16 18:10:58 by jherrald         ###   ########.fr       */
+/*   Updated: 2019/12/17 12:14:37 by jherrald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 # include <string.h>
 # include <stdlib.h>
 # include <limits.h>
+# include <stdarg.h>
 
+int	printf(const char *, ...);
 
 #endif
