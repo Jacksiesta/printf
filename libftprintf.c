a8f241f97f246532c156b25862ea823cad97fc56
Jacqueline Herrald <jherrald@e-r4-p2.s19.be>
2019-12-18 20:25:19 +0100

    update

--- libftprintf.c
+++ libftprintf.c
@@ -6,7 +6,7 @@
 /*   By: jherrald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 16:48:44 by jherrald          #+#    #+#             */
-/*   Updated: 2019/12/17 16:41:30 by jherrald         ###   ########.fr       */
+/*   Updated: 2019/12/18 20:24:34 by jherrald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,27 @@ char	ft_s(char *s, va_list ap)
 	return (0);
 }
 
+char	ft_c(char c, va_list ap)
+{
+	char	a;
+
+	a = va_arg(ap, int);
+	write(1, &a, 1);
+	return (0);
+}
+
+int		ft_d(int nombre, va_list ap)
+{
+	return (0);
+}
+
 int		ft_printf(const char *coucou, ...)
 {
 	int		x;
 	va_list	ap;
 	char	*value;
 	char	a;
+	int		nbr;
 
 	x = 0;
 	va_start(ap, coucou); 
@@ -46,28 +61,23 @@ int		ft_printf(const char *coucou, ...)
 		{
 			if (coucou[x + 1] == 's')
 				ft_s(value, ap);
-				break;		
 			if (coucou[x + 1] == 'c')
-			{
-				a = va_arg(ap, int);
-				write(1, &a, 1);
-				return (0);
-			}
+				ft_c(a, ap);
+			if (coucou[x + 1] == 'd')
+				ft_d(nbr, ap);
+			x = x + 2;
 		}
 		write(1, &coucou[x], 1);
 		x++;
 	}
-
-	va_end(ap);
 	return (0);
 }
 
 int main()
 {
 	char 	*temp = "kikouuuu";
-	char	*oups = "what";
-	char	a = 'c';
-	ft_printf("test :%s", oups);
-//	ft_printf("test :%c", a);
-		
+	char	*oups = "wHaT";
+	char	a = 'B';
+	ft_printf("what : %s WHAT \n", oups);
+	printf("what : %s WHAT \n", oups);
 }
