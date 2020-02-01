1bc5c490273e8d91c24f36dc4af30b4f51b323e0
Jacqueline Herrald <jherrald@e-r7-p4.s19.be>
2020-02-01 20:23:02 +0100

    cleaned %d (restarted) 52 Ok

--- libftprintf.c
+++ libftprintf.c
@@ -6,7 +6,7 @@
 /*   By: jherrald <jherrald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 16:48:44 by jherrald          #+#    #+#             */
-/*   Updated: 2020/01/31 15:53:37 by jherrald         ###   ########.fr       */
+/*   Updated: 2020/02/01 20:22:23 by jherrald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,58 +66,110 @@ char	*parser(va_list ap, const char *str, t_flag *flag) // activates flags
     return (0);
 }
 
-char    *convers_d(va_list ap, t_flag *flag)
+char	*convers_d(va_list ap, t_flag *flag)
 {
-	char		*init;
-	int			num;
-	int			size;
-	int			neg;
-	char		*final;
+	char	*init;
+	int 	num;
+	int		size;
+	int		neg;
+	char	*final;
 
 	num = va_arg(ap, int);
 	neg = (num < 0) ? 1 : 0;
 	num = (num < 0) ? -num : num;
-	init = ft_d(num); // number stocked in string
+	init = ft_d(num);
 	size = ft_strlen(init);
-//	printf("_________\nsize init is %d\n", size);
-//	printf(":%d zero flag \n", flag->zero_flag);	
-//	printf(":%d minus flag \n", flag->minus_flag);
-//	printf(":%d width \n", flag->width);
-//	printf(":%d precision \n", flag->precision);
-//	printf("init string is :%s\n_______\n", init);
 	final = ft_strdup("");
+	if (flag->precision)
+	{
+		if (flag->precision > size)
+			final = ft_strjoin(pad_maker('0', flag->precision - size), init);
+		else
+			final = ft_strdup(init);
+	}
 	if (neg)
-		final = ft_strdup(pad_maker('-', 1));
-	if (flag->precision && flag->precision > size) //add 0s precision
-		final = ft_strjoin(final, pad_maker('0', flag->precision - size));
-	final = ft_strjoin(final, init);
-//	if (flag->zero_flag)
-//	{
-//		final = ft_strjoin(final, ft_strjoin(pad_maker('0', flag->width - size), init));
-//		return (final);
-//	}
-	if (flag->minus_flag && flag->width > size)
+		final = ft_strjoin(ft_strdup(pad_maker('-', 1)), final);
+	if (flag->minus_flag)
 	{
-		if (flag->precision != -1)
-			final = ft_strjoin(final, pad_maker(' ', flag->width - flag->precision - neg));
-		else
+		if (flag->width > size)
 			final = ft_strjoin(final, pad_maker(' ', flag->width - size - neg));
-		return (final);
 	}
-//	if (flag->zero_flag)
-//	{
-//		final = ft_strjoin(pad_maker('0', flag->width - size), final);
-//		return (final);
-//	}
-	if (flag->width && flag->width > flag->precision && flag->width > size) //add sp for width
+	if (flag->zero_flag)
 	{
-		if (flag->precision == -1)
-					final = ft_strjoin(pad_maker(' ', flag->width - size - neg), final);
-		else
-			final = ft_strjoin(pad_maker(' ', flag->width - flag->precision - neg), final);
+		if (flag->width > size)
+			final = ft_strjoin(pad_maker('0', flag->width - size), final);
 	}
+	if (flag->width && !flag->minus_flag && !flag->zero_flag)
+	{
+		if (flag->width > size)
+			final = ft_strjoin(pad_maker(' ', flag->width - size - neg), final);
+	}
+
 	return (final);
 }
+
+
+
+//char    *convers_d(va_list ap, t_flag *flag)
+//{
+//	char		*init;
+//	int			num;
+//	int			size;
+//	int			neg;
+//	char		*final;
+//
+//	num = va_arg(ap, int);
+//	neg = (num < 0) ? 1 : 0;
+//	num = (num < 0) ? -num : num;
+//	init = ft_d(num); // number stocked in string
+//	size = ft_strlen(init);
+////	printf("_________\nsize init is %d\n", size);
+////	printf(":%d zero flag \n", flag->zero_flag);	
+////	printf(":%d minus flag \n", flag->minus_flag);
+////	printf(":%d width \n", flag->width);
+////	printf(":%d precision \n", flag->precision);
+////	printf("init string is :%s\n_______\n", init);
+//	final = ft_strdup("");
+////	if (neg)
+////		final = ft_strdup(pad_maker('-', 1));
+//	if (flag->precision && flag->precision > size) //add 0s precision
+//	{
+//		if (neg)
+//			final = ft_strjoin(ft_strdup(pad_maker('-', 1)), pad_maker('0', flag->precision - size));
+//		else
+//			final = ft_strjoin(final, pad_maker('0', flag->precision - size));
+//	}
+//	final = ft_strjoin(final, init);
+////	if (flag->zero_flag)
+////	{
+////		final = ft_strjoin(final, ft_strjoin(pad_maker('0', flag->width - size), init));
+////		return (final);
+////	}
+//	if (flag->minus_flag && flag->width > size)
+//	{
+//		if (flag->precision != -1)
+//			final = ft_strjoin(final, pad_maker(' ', flag->width - flag->precision - neg));
+//		else
+//			final = ft_strjoin(final, pad_maker(' ', flag->width - size - neg));
+//		return (final);
+//	}
+//	if (flag->zero_flag && flag->width > size)
+//	{
+//		final = ft_strjoin(pad_maker('0', flag->width - size - neg), final);
+//		if (neg)
+//			final = ft_strjoin(ft_strdup(pad_maker('-', 1)), final);
+//		return (final);
+//	}
+////	}
+//	if (flag->width && flag->width > flag->precision && flag->width > size) //add sp for width
+//	{
+//		if (flag->precision == -1)
+//					final = ft_strjoin(pad_maker(' ', flag->width - size - neg), final);
+//		else
+//			final = ft_strjoin(pad_maker(' ', flag->width - flag->precision - neg), final);
+//	}
+//	return (final);
+//}
 
 int		size_percent(const char *str)
 {
@@ -185,16 +237,10 @@ int		ft_printf(const char *coucou, ...)
 
 //int main()
 //{
-//	int		numba = 123;
+////	int		numba = 123;
 //
 ////	printf("%d\n", ft_printf("YES%7d", 33));
-//	ft_printf("%07d", -54);
-//	printf("\n%07d\n", -54);
-////	printf("\n%-1d", 33);
-////	printf("%.10d\n", 33);
-////	printf("3.%-5d\n", -33);
-////	printf("2.%-15.7d\n", 33);
-////	printf("1.%-5.7d\n", -33);
-////	printf("\n%7d\n", 33);
+//	ft_printf("%05d", -43);
+//	printf("\n%05d\n", -43);
 //	return (0);
 //}
